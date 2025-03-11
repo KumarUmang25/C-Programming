bdd5c3343229dcb09d6e27d825bc179bcea06681
Kumar Umang <umang7622@gmail.com>
2025-03-11 19:47:51 +0530

    Update tBill.c

--- tBill.c
+++ tBill.c
@@ -6,17 +6,16 @@ int main(){
     scanf("%d" , &calls);
     switch(calls){
         case 1...150:TotalBill=0;
-                    printf("Total Bill = %d",TotalBill);
+                    printf("Total Bill = %0.2f",TotalBill);
                     break;
         case 151...250:TotalBill=(calls-150)*0.9;
-                       printf("Total Bill = %d",TotalBill);
+                       printf("Total Bill = %0.2f",TotalBill);
                        break;
         case 251...400:TotalBill=100*0.9+(calls-250)*1.2;
-                       printf("Total Bill = %d",TotalBill);
+                       printf("Total Bill = %0.2f",TotalBill);
                        break;
         default:TotalBill=(100*0.9)+(150*1.2)+(calls-400)*1.5;
-                printf("Total Bill = %d",TotalBill)
-                break;
+                printf("Total Bill = %0.2f",TotalBill);
     }
     return 0;
 }
